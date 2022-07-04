94eb7978f54fe133750649866b8ea744076c8e20
Victor Carreno <vmanuelcar@hotmail.com>
2022-07-04 14:51:18 -0500

    Created images logic

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ async function pokemonFetch(pokemon = 1){
         const response = await fetch(POKEMON_API + pokemon)
         const data = await response.json()
         console.log(data)
+        pokemonImg.src = data.sprites.other["official-artwork"].front_default
     } catch (error){
         console.log(error)
     }
 }
+pokemonFetch()
 
 function displayPokemon(){
 
