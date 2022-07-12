0eb759dbd060e6ffb478a75c9d67bbccace321be
Victor Carreno <vmanuelcar@hotmail.com>
2022-07-12 12:41:23 -0500

    New pokemon's list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const POKEMON_API = "https://pokeapi.co/api/v2/pokemon/";
+const POKEMON_V2_API = "https://pokeapi.co/api/v2/pokemon/";
+const POKEMON_SPECIES_API = "https://pokeapi.co/api/v2/pokemon-species/";
 
 const pokemonName = document.getElementById("pokemonName");
 const pokemonImg = document.getElementById("pokemonImg");
@@ -10,19 +11,30 @@ const statBox_SP_DEFENSE = document.getElementById("stat-special-defense");
 const statBox_SPEED = document.getElementById("stat-speed");
 const pokemonDescription = document.getElementById("pokemon-description");
 
-const searcherIcon = document.getElementById("searcherIcon")
+const searcherIcon = document.getElementById("searcherIcon");
 const searcherContainer = document.querySelector(".searcher-container");
 const inputSearcher = document.getElementById("searcher-input");
-const closeModalDiv = document.getElementById("closeModalDiv")
+const closeModalDiv = document.getElementById("closeModalDiv");
+
+const pokemonsListContainer = document.getElementById("pokemonsListContainer");
 
 closeModalDiv.onclick = closeModalSearcher
 searcherIcon.onclick = openModalSearcher
 window.addEventListener("DOMContentLoaded", closeModalSearcher, false)
 
-async function pokemonFetch(pokemon = 1){
+async function pokemonFetch(info){
     try {
-        const response = await fetch(POKEMON_API + pokemon)
+        const response = await fetch(info)
         const data = await response.json()
+        return data;
+    } catch (error){
+        console.log(error)
+    }
+}
+
+async function mainPokemon(pokemon = 1){
+    try {
+        const data = await pokemonFetch(POKEMON_V2_API + pokemon)
         console.log(data)
 
         // Adding images and info to HTML
@@ -35,36 +47,69 @@ async function pokemonFetch(pokemon = 1){
         statBox_SP_ATTACK.textContent = data.stats[3].base_stat
         statBox_SP_DEFENSE.textContent = data.stats[4].base_stat
         statBox_SPEED.textContent = data.stats[5].base_stat
-        pokemonDescription.textContent = await descriptionPokemon(pokemon)
+        const englishDescription = await pokemonFetch(POKEMON_SPECIES_API + pokemon);
+        const englishText = englishDescription.flavor_text_entries.filter(object => object.language.name === "en")
+        pokemonDescription.textContent = englishText[0].flavor_text.replace("\f"," ")
 
     } catch (error){
         console.log(error)
     }
 }
-pokemonFetch()
+mainPokemon()
 
-async function descriptionPokemon(pokemon = 1){
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokemon)
-        const data = await response.json()
-        const englishText = data.flavor_text_entries.filter(object => object.language.name === "en")
-        return englishText[0].flavor_text.replace("\f"," ")
-    } catch (error) {
-        console.log(error)
-    }
+let pokemonListCount = 0;
+async function pokemonList(){
+    const firstData = await pokemonFetch(POKEMON_V2_API + "?limit=10&offset=" + pokemonListCount);
+    console.log(firstData)
+    firstData.results.forEach(async (data_element) => {
+        const dataPokemon = await pokemonFetch(data_element.url);
+
+        const pokemonArticleContainer = document.createElement("article");
+        const pokemonImg = document.createElement("img");
+        pokemonImg.src = dataPokemon.sprites.front_default;
+        pokemonImg.alt = "Image of " + dataPokemon.name;
+        pokemonArticleContainer.appendChild(pokemonImg);
+
+        const pokemonSpanName = document.createElement("span");
+        pokemonSpanName.textContent = dataPokemon.name;
+        pokemonArticleContainer.appendChild(pokemonSpanName);
+
+        const pokemonSpanId = document.createElement("span");
+        pokemonSpanId.textContent = dataPokemon.id;
+        pokemonArticleContainer.appendChild(pokemonSpanId);
+
+        dataPokemon.types.forEach(element => {
+            const pokemonSpan = document.createElement("span");
+            pokemonSpan.textContent = element.type.name;
+            pokemonSpan.classList.add("list-pokemon-type");
+            pokemonSpan.classList.add(element.type.name);
+            pokemonArticleContainer.appendChild(pokemonSpan);
+        })
+
+        pokemonsListContainer.appendChild(pokemonArticleContainer)
+    })
 }
+pokemonList()
+
+// function newPagePokemonList(next = true){
+//     if (!pokemonListCount < 0 || !pokemonListCount > 890){
+//     next
+//     } else {
+//         alert("There's no pokémons to show 😢")
+//     }
+// }
 
 function randomPokemon(){
     let random = 0;
     const rnd = () => random = Math.floor(Math.random() * 900)
     rnd()
     if (random > 898) rnd()
-    else pokemonFetch(random)
+    else mainPokemon(random)
 }
 
 function searchPokemon(){
     const value = inputSearcher.value.toLowerCase()
-    pokemonFetch(value)
+    mainPokemon(value)
 }
 
 function openModalSearcher() {
@@ -75,5 +120,5 @@ function openModalSearcher() {
 function closeModalSearcher() {
     searcherContainer.classList.remove("display")
     searcherContainer.classList.add("no-display")
-    inputSearcher.value = ""
+    inputSearcher.value = "";
 }
